932005a128ee9d006cb0138ebde221d6661425c5
fuzioncloud <zzaharris@fuzioncloud.com>
2017-10-08 09:55:19 +0000

    automatically backed up by learn

---
+++ arrays.js
@@ -0,0 +1,97 @@
+var ingredient1 =  "bread"
+var bread = [
+  ingredient1
+]
+
+var ingredient2 =  "mild cheese"
+var ingredient3 =  "sharp cheese"
+var cheeses = [
+  mild = [
+    ingredient2
+  ],
+  sharp = [
+    ingredient3
+  ]
+]
+var ingredient4 =  "butter"
+var butter = [
+  ingredient4
+]
+var ingredient5 =  "tomato"
+var fruits = [
+  tomato = [
+    ingredient5
+  ],
+  "apple",
+  "orange",
+  "kiwi"
+]
+var ingredient6 =  "garlic"
+var vegitables = [
+  garlic = [
+    ingredient6
+  ],
+  "broccoli",
+  "celery",
+  "carrot"
+]
+// var ingredient7 =  ""
+
+var grilledCheeseIngredients = [
+  "bread",
+  "mild cheese",
+  "sharp cheese",
+  "butter",
+  "tomato",
+  "garlic"
+]
+
+var tomatoSauceIngredients = [
+  "tomato",
+  "garlic",
+  "olive oil",
+  "basil",
+  "oregano"
+]
+
+// ARRAY CREATION #1
+// First way to Create arrays is using a list of values in a pair of square brackets "[]" assigned to a variable. These arrays are called "array literals." i.e.:
+      // var myArray = [element0, element1, element2, ..., elementN ];
+
+// Examples
+/**
+
+```javascript
+
+var primeNumbers = [2, 3, 5, 7, 11, 13, 17, 19, 23, 29, 31, 37];
+
+var tvShows = ["game of thrones",
+      "true detective",
+      "the good wife",
+      "empire"
+    ];
+
+var empty = [];
+```
+**/
+
+var weirdGreeting = [
+      "he", 110, "w", 0, "r", 1, "d" ];
+
+
+// ARRAY CREATION #2
+// The Array Constructor is the Second way to Create an array
+/**
+
+```javascript
+var evenNumbers = new Array();
+
+**/
+
+function addElementToBeginningOfArray() {
+
+}
+
+function destructivelyAddElementToBeginningOfArray() {
+
+}
